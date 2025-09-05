d6f62ff5c104b12123fe892b9afcb0854e08a940
eduardogoriajr <eduardogoriajr@gmail.com>
2025-09-06 06:16:57 +0800

    Kiosk

---
+++ kiosk/kiosk/Form1.Designer.vb
@@ -0,0 +1,443 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        chkBurg = New CheckBox()
+        chkSpag = New CheckBox()
+        chkFries = New CheckBox()
+        chkPizza = New CheckBox()
+        chkTea = New CheckBox()
+        chkCoke = New CheckBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        burgQuan = New TextBox()
+        spagQuan = New TextBox()
+        pizzaQuan = New TextBox()
+        friesQuan = New TextBox()
+        cokeQuan = New TextBox()
+        teaQuan = New TextBox()
+        btnMin1 = New Button()
+        txtTotal = New TextBox()
+        txtPayment = New TextBox()
+        txtChange = New TextBox()
+        btnSubmit = New Button()
+        btnPay = New Button()
+        btnClear = New Button()
+        btnMin2 = New Button()
+        btnMin4 = New Button()
+        btnMin3 = New Button()
+        btnMin6 = New Button()
+        btnMin5 = New Button()
+        btnAdd4 = New Button()
+        btnAdd3 = New Button()
+        btnAdd2 = New Button()
+        btnAdd1 = New Button()
+        btnAdd6 = New Button()
+        btnAdd5 = New Button()
+        SuspendLayout()
+        ' 
+        ' chkBurg
+        ' 
+        chkBurg.AutoSize = True
+        chkBurg.Location = New Point(117, 53)
+        chkBurg.Name = "chkBurg"
+        chkBurg.Size = New Size(90, 29)
+        chkBurg.TabIndex = 0
+        chkBurg.Text = "Burger"
+        chkBurg.UseVisualStyleBackColor = True
+        ' 
+        ' chkSpag
+        ' 
+        chkSpag.AutoSize = True
+        chkSpag.Location = New Point(117, 106)
+        chkSpag.Name = "chkSpag"
+        chkSpag.Size = New Size(114, 29)
+        chkSpag.TabIndex = 1
+        chkSpag.Text = "Spaghetti"
+        chkSpag.UseVisualStyleBackColor = True
+        ' 
+        ' chkFries
+        ' 
+        chkFries.AutoSize = True
+        chkFries.Location = New Point(117, 216)
+        chkFries.Name = "chkFries"
+        chkFries.Size = New Size(74, 29)
+        chkFries.TabIndex = 3
+        chkFries.Text = "Fries"
+        chkFries.UseVisualStyleBackColor = True
+        ' 
+        ' chkPizza
+        ' 
+        chkPizza.AutoSize = True
+        chkPizza.Location = New Point(117, 164)
+        chkPizza.Name = "chkPizza"
+        chkPizza.Size = New Size(77, 29)
+        chkPizza.TabIndex = 2
+        chkPizza.Text = "Pizza"
+        chkPizza.UseVisualStyleBackColor = True
+        ' 
+        ' chkTea
+        ' 
+        chkTea.AutoSize = True
+        chkTea.Location = New Point(494, 104)
+        chkTea.Name = "chkTea"
+        chkTea.Size = New Size(90, 29)
+        chkTea.TabIndex = 5
+        chkTea.Text = "Ice Tea"
+        chkTea.UseVisualStyleBackColor = True
+        ' 
+        ' chkCoke
+        ' 
+        chkCoke.AutoSize = True
+        chkCoke.Location = New Point(494, 52)
+        chkCoke.Name = "chkCoke"
+        chkCoke.Size = New Size(78, 29)
+        chkCoke.TabIndex = 4
+        chkCoke.Text = "Coke"
+        chkCoke.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(178, 288)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(49, 25)
+        Label1.TabIndex = 6
+        Label1.Text = "Total"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(178, 330)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(80, 25)
+        Label2.TabIndex = 7
+        Label2.Text = "Payment"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(178, 375)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(72, 25)
+        Label3.TabIndex = 8
+        Label3.Text = "Change"
+        ' 
+        ' burgQuan
+        ' 
+        burgQuan.Enabled = False
+        burgQuan.Location = New Point(302, 54)
+        burgQuan.Name = "burgQuan"
+        burgQuan.Size = New Size(59, 31)
+        burgQuan.TabIndex = 9
+        ' 
+        ' spagQuan
+        ' 
+        spagQuan.Enabled = False
+        spagQuan.Location = New Point(302, 104)
+        spagQuan.Name = "spagQuan"
+        spagQuan.Size = New Size(59, 31)
+        spagQuan.TabIndex = 10
+        ' 
+        ' pizzaQuan
+        ' 
+        pizzaQuan.Enabled = False
+        pizzaQuan.Location = New Point(302, 162)
+        pizzaQuan.Name = "pizzaQuan"
+        pizzaQuan.Size = New Size(59, 31)
+        pizzaQuan.TabIndex = 11
+        ' 
+        ' friesQuan
+        ' 
+        friesQuan.Enabled = False
+        friesQuan.Location = New Point(302, 216)
+        friesQuan.Name = "friesQuan"
+        friesQuan.Size = New Size(59, 31)
+        friesQuan.TabIndex = 12
+        ' 
+        ' cokeQuan
+        ' 
+        cokeQuan.Enabled = False
+        cokeQuan.Location = New Point(639, 54)
+        cokeQuan.Name = "cokeQuan"
+        cokeQuan.Size = New Size(59, 31)
+        cokeQuan.TabIndex = 13
+        ' 
+        ' teaQuan
+        ' 
+        teaQuan.Enabled = False
+        teaQuan.Location = New Point(639, 106)
+        teaQuan.Name = "teaQuan"
+        teaQuan.Size = New Size(59, 31)
+        teaQuan.TabIndex = 14
+        ' 
+        ' btnMin1
+        ' 
+        btnMin1.Enabled = False
+        btnMin1.Location = New Point(259, 52)
+        btnMin1.Name = "btnMin1"
+        btnMin1.Size = New Size(37, 34)
+        btnMin1.TabIndex = 15
+        btnMin1.Text = "-"
+        btnMin1.UseVisualStyleBackColor = True
+        ' 
+        ' txtTotal
+        ' 
+        txtTotal.Enabled = False
+        txtTotal.Location = New Point(265, 285)
+        txtTotal.Name = "txtTotal"
+        txtTotal.Size = New Size(319, 31)
+        txtTotal.TabIndex = 16
+        ' 
+        ' txtPayment
+        ' 
+        txtPayment.Location = New Point(265, 327)
+        txtPayment.Name = "txtPayment"
+        txtPayment.Size = New Size(319, 31)
+        txtPayment.TabIndex = 17
+        ' 
+        ' txtChange
+        ' 
+        txtChange.Enabled = False
+        txtChange.Location = New Point(265, 372)
+        txtChange.Name = "txtChange"
+        txtChange.Size = New Size(319, 31)
+        txtChange.TabIndex = 18
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(610, 283)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(112, 34)
+        btnSubmit.TabIndex = 19
+        btnSubmit.Text = "Submit"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' btnPay
+        ' 
+        btnPay.Location = New Point(610, 327)
+        btnPay.Name = "btnPay"
+        btnPay.Size = New Size(112, 34)
+        btnPay.TabIndex = 20
+        btnPay.Text = "Pay"
+        btnPay.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(610, 370)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(112, 34)
+        btnClear.TabIndex = 21
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnMin2
+        ' 
+        btnMin2.Enabled = False
+        btnMin2.Location = New Point(259, 104)
+        btnMin2.Name = "btnMin2"
+        btnMin2.Size = New Size(37, 34)
+        btnMin2.TabIndex = 22
+        btnMin2.Text = "-"
+        btnMin2.UseVisualStyleBackColor = True
+        ' 
+        ' btnMin4
+        ' 
+        btnMin4.Enabled = False
+        btnMin4.Location = New Point(259, 212)
+        btnMin4.Name = "btnMin4"
+        btnMin4.Size = New Size(37, 34)
+        btnMin4.TabIndex = 24
+        btnMin4.Text = "-"
+        btnMin4.UseVisualStyleBackColor = True
+        ' 
+        ' btnMin3
+        ' 
+        btnMin3.Enabled = False
+        btnMin3.Location = New Point(259, 160)
+        btnMin3.Name = "btnMin3"
+        btnMin3.Size = New Size(37, 34)
+        btnMin3.TabIndex = 23
+        btnMin3.Text = "-"
+        btnMin3.UseVisualStyleBackColor = True
+        ' 
+        ' btnMin6
+        ' 
+        btnMin6.Enabled = False
+        btnMin6.Location = New Point(596, 104)
+        btnMin6.Name = "btnMin6"
+        btnMin6.Size = New Size(37, 34)
+        btnMin6.TabIndex = 26
+        btnMin6.Text = "-"
+        btnMin6.UseVisualStyleBackColor = True
+        ' 
+        ' btnMin5
+        ' 
+        btnMin5.Enabled = False
+        btnMin5.Location = New Point(596, 52)
+        btnMin5.Name = "btnMin5"
+        btnMin5.Size = New Size(37, 34)
+        btnMin5.TabIndex = 25
+        btnMin5.Text = "-"
+        btnMin5.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd4
+        ' 
+        btnAdd4.Enabled = False
+        btnAdd4.Location = New Point(367, 213)
+        btnAdd4.Name = "btnAdd4"
+        btnAdd4.Size = New Size(37, 34)
+        btnAdd4.TabIndex = 30
+        btnAdd4.Text = "+"
+        btnAdd4.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd3
+        ' 
+        btnAdd3.Enabled = False
+        btnAdd3.Location = New Point(367, 161)
+        btnAdd3.Name = "btnAdd3"
+        btnAdd3.Size = New Size(37, 34)
+        btnAdd3.TabIndex = 29
+        btnAdd3.Text = "+"
+        btnAdd3.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd2
+        ' 
+        btnAdd2.Enabled = False
+        btnAdd2.Location = New Point(367, 105)
+        btnAdd2.Name = "btnAdd2"
+        btnAdd2.Size = New Size(37, 34)
+        btnAdd2.TabIndex = 28
+        btnAdd2.Text = "+"
+        btnAdd2.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd1
+        ' 
+        btnAdd1.Enabled = False
+        btnAdd1.Location = New Point(367, 53)
+        btnAdd1.Name = "btnAdd1"
+        btnAdd1.Size = New Size(37, 34)
+        btnAdd1.TabIndex = 27
+        btnAdd1.Text = "+"
+        btnAdd1.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd6
+        ' 
+        btnAdd6.Enabled = False
+        btnAdd6.Location = New Point(704, 106)
+        btnAdd6.Name = "btnAdd6"
+        btnAdd6.Size = New Size(37, 34)
+        btnAdd6.TabIndex = 32
+        btnAdd6.Text = "+"
+        btnAdd6.UseVisualStyleBackColor = True
+        ' 
+        ' btnAdd5
+        ' 
+        btnAdd5.Enabled = False
+        btnAdd5.Location = New Point(704, 54)
+        btnAdd5.Name = "btnAdd5"
+        btnAdd5.Size = New Size(37, 34)
+        btnAdd5.TabIndex = 31
+        btnAdd5.Text = "+"
+        btnAdd5.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnAdd6)
+        Controls.Add(btnAdd5)
+        Controls.Add(btnAdd4)
+        Controls.Add(btnAdd3)
+        Controls.Add(btnAdd2)
+        Controls.Add(btnAdd1)
+        Controls.Add(btnMin6)
+        Controls.Add(btnMin5)
+        Controls.Add(btnMin4)
+        Controls.Add(btnMin3)
+        Controls.Add(btnMin2)
+        Controls.Add(btnClear)
+        Controls.Add(btnPay)
+        Controls.Add(btnSubmit)
+        Controls.Add(txtChange)
+        Controls.Add(txtPayment)
+        Controls.Add(txtTotal)
+        Controls.Add(btnMin1)
+        Controls.Add(teaQuan)
+        Controls.Add(cokeQuan)
+        Controls.Add(friesQuan)
+        Controls.Add(pizzaQuan)
+        Controls.Add(spagQuan)
+        Controls.Add(burgQuan)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(chkTea)
+        Controls.Add(chkCoke)
+        Controls.Add(chkFries)
+        Controls.Add(chkPizza)
+        Controls.Add(chkSpag)
+        Controls.Add(chkBurg)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents chkBurg As CheckBox
+    Friend WithEvents chkSpag As CheckBox
+    Friend WithEvents chkFries As CheckBox
+    Friend WithEvents chkPizza As CheckBox
+    Friend WithEvents chkTea As CheckBox
+    Friend WithEvents chkCoke As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents burgQuan As TextBox
+    Friend WithEvents spagQuan As TextBox
+    Friend WithEvents pizzaQuan As TextBox
+    Friend WithEvents friesQuan As TextBox
+    Friend WithEvents cokeQuan As TextBox
+    Friend WithEvents teaQuan As TextBox
+    Friend WithEvents btnMin1 As Button
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents txtPayment As TextBox
+    Friend WithEvents txtChange As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnPay As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnMin2 As Button
+    Friend WithEvents btnMin4 As Button
+    Friend WithEvents btnMin3 As Button
+    Friend WithEvents btnMin6 As Button
+    Friend WithEvents btnMin5 As Button
+    Friend WithEvents btnAdd4 As Button
+    Friend WithEvents btnAdd3 As Button
+    Friend WithEvents btnAdd2 As Button
+    Friend WithEvents btnAdd1 As Button
+    Friend WithEvents btnAdd6 As Button
+    Friend WithEvents btnAdd5 As Button
+
+End Class
---
+++ kiosk/kiosk/Form1.vb
@@ -0,0 +1,225 @@
+﻿Public Class Form1
+    Dim burgQuantity, spagQuantity, pizzaQuantity, friestQuantity, cokeQuantity, teaQuantity As Integer
+    Dim total, payment, change As Double
+
+    Private Sub Burger_CheckedChanged(sender As Object, e As EventArgs) Handles chkBurg.CheckedChanged
+        If chkBurg.Checked Then
+            burgQuan.Enabled = True
+            btnAdd1.Enabled = True
+            btnMin1.Enabled = True
+            burgQuan.Text = 1
+        Else
+            burgQuan.Enabled = False
+            btnAdd1.Enabled = False
+            btnMin1.Enabled = False
+            burgQuan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnAdd1_Click(sender As Object, e As EventArgs) Handles btnAdd1.Click
+        burgQuan.Text += 1
+    End Sub
+
+    Private Sub btnMin1_Click(sender As Object, e As EventArgs) Handles btnMin1.Click
+        burgQuan.Text -= 1
+        If burgQuan.Text < 1 Then
+            chkBurg.Checked = False
+        End If
+    End Sub
+
+    Private Sub Spag_CheckedChanged(sender As Object, e As EventArgs) Handles chkSpag.CheckedChanged
+        If chkSpag.Checked Then
+            spagQuan.Enabled = True
+            btnAdd2.Enabled = True
+            btnMin2.Enabled = True
+            spagQuan.Text = 1
+        Else
+            spagQuan.Enabled = False
+            btnAdd2.Enabled = False
+            btnMin2.Enabled = False
+            spagQuan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnAdd2_Click(sender As Object, e As EventArgs) Handles btnAdd2.Click
+        spagQuan.Text += 1
+    End Sub
+
+    Private Sub btnMin2_Click(sender As Object, e As EventArgs) Handles btnMin2.Click
+        spagQuan.Text -= 1
+        If spagQuan.Text < 1 Then
+            chkSpag.Checked = False
+        End If
+    End Sub
+
+    Private Sub Pizza_CheckedChanged(sender As Object, e As EventArgs) Handles chkPizza.CheckedChanged
+        If chkPizza.Checked Then
+            pizzaQuan.Enabled = True
+            btnAdd3.Enabled = True
+            btnMin3.Enabled = True
+            pizzaQuan.Text = 1
+        Else
+            pizzaQuan.Enabled = False
+            btnAdd3.Enabled = False
+            btnMin3.Enabled = False
+            pizzaQuan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnAdd3_Click(sender As Object, e As EventArgs) Handles btnAdd3.Click
+        pizzaQuan.Text += 1
+    End Sub
+
+    Private Sub btnMin3_Click(sender As Object, e As EventArgs) Handles btnMin3.Click
+        pizzaQuan.Text -= 1
+        If pizzaQuan.Text < 1 Then
+            chkPizza.Checked = False
+        End If
+    End Sub
+
+    Private Sub Fries_CheckedChanged(sender As Object, e As EventArgs) Handles chkFries.CheckedChanged
+        If chkFries.Checked Then
+            friesQuan.Enabled = True
+            btnAdd4.Enabled = True
+            btnMin4.Enabled = True
+            friesQuan.Text = 1
+        Else
+            friesQuan.Enabled = False
+            btnAdd4.Enabled = False
+            btnMin4.Enabled = False
+            friesQuan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnAdd4_Click(sender As Object, e As EventArgs) Handles btnAdd4.Click
+        friesQuan.Text += 1
+    End Sub
+
+    Private Sub btnMin4_Click(sender As Object, e As EventArgs) Handles btnMin4.Click
+        friesQuan.Text -= 1
+        If friesQuan.Text < 1 Then
+            chkFries.Checked = False
+        End If
+    End Sub
+
+    Private Sub Coke_CheckedChanged(sender As Object, e As EventArgs) Handles chkCoke.CheckedChanged
+        If chkCoke.Checked Then
+            cokeQuan.Enabled = True
+            btnAdd5.Enabled = True
+            btnMin5.Enabled = True
+            cokeQuan.Text = 1
+        Else
+            cokeQuan.Enabled = False
+            btnAdd5.Enabled = False
+            btnMin5.Enabled = False
+            cokeQuan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnAdd5_Click(sender As Object, e As EventArgs) Handles btnAdd5.Click
+        cokeQuan.Text += 1
+    End Sub
+
+    Private Sub btnMin5_Click(sender As Object, e As EventArgs) Handles btnMin5.Click
+        cokeQuan.Text -= 1
+        If cokeQuan.Text < 1 Then
+            chkCoke.Checked = False
+        End If
+    End Sub
+
+    Private Sub Tea_CheckedChanged(sender As Object, e As EventArgs) Handles chkTea.CheckedChanged
+        If chkTea.Checked Then
+            teaQuan.Enabled = True
+            btnAdd6.Enabled = True
+            btnMin6.Enabled = True
+            teaQuan.Text = 1
+        Else
+            teaQuan.Enabled = False
+            btnAdd6.Enabled = False
+            btnMin6.Enabled = False
+            teaQuan.Clear()
+        End If
+    End Sub
+
+    Private Sub btnAdd6_Click(sender As Object, e As EventArgs) Handles btnAdd6.Click
+        teaQuan.Text += 1
+    End Sub
+
+    Private Sub btnMin6_Click(sender As Object, e As EventArgs) Handles btnMin6.Click
+        teaQuan.Text -= 1
+        If teaQuan.Text < 1 Then
+            chkTea.Checked = False
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim answer As DialogResult
+        answer = MessageBox.Show("Are you sure you want to proceed?", "Submit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If answer = DialogResult.Yes Then
+            If chkBurg.Checked Then
+                burgQuantity = burgQuan.Text
+            Else
+                burgQuantity = 0
+            End If
+
+            If chkSpag.Checked Then
+                spagQuantity = spagQuan.Text
+            Else
+                spagQuantity = 0
+            End If
+
+            If chkPizza.Checked Then
+                pizzaQuantity = pizzaQuan.Text
+            Else
+                pizzaQuantity = 0
+            End If
+
+            If chkFries.Checked Then
+                friestQuantity = friesQuan.Text
+            Else
+                friestQuantity = 0
+            End If
+
+            If chkCoke.Checked Then
+                cokeQuantity = cokeQuan.Text
+            Else
+                cokeQuantity = 0
+            End If
+
+            If chkTea.Checked Then
+                teaQuantity = teaQuan.Text
+            Else
+                teaQuantity = 0
+            End If
+
+            total = (burgQuantity * 50) + (spagQuantity * 70) + (pizzaQuantity * 525) + (friestQuantity * 45) + (cokeQuantity * 29) + (teaQuantity * 35)
+            txtTotal.Text = total
+        End If
+    End Sub
+
+    Private Sub btnPay_Click(sender As Object, e As EventArgs) Handles btnPay.Click
+        payment = txtPayment.Text
+        If payment >= total Then
+            txtChange.Text = (payment - total)
+        Else
+            MsgBox("Insufficient Balance")
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Dim answer As DialogResult
+        answer = MessageBox.Show("Are you sure you want to clear?", "Clear", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If answer = DialogResult.Yes Then
+            txtTotal.Clear()
+            txtPayment.Clear()
+            txtChange.Clear()
+            chkBurg.Checked = False
+            chkSpag.Checked = False
+            chkPizza.Checked = False
+            chkFries.Checked = False
+            chkCoke.Checked = False
+            chkTea.Checked = False
+        End If
+    End Sub
+End Class
